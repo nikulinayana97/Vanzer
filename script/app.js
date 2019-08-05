c70b5b7a2e6d4b4f5eee1a7516d0ad09d81b6e88
nikulinayana97 <nikulina.yana97@gmail.com>
2019-08-05 17:26:10 +0300

    adaptive menu and hero

--- script/app.js
+++ script/app.js
@@ -1,16 +1,41 @@
-$(document).ready(function() {
-		$(document).delegate('.header__menu', 'click', function(event){
-			$(this).addClass('oppenned');
-			event.stopPropagation();
-		})
-		$(document).delegate('body', 'click', function(event) {
-			$('.header__menu').removeClass('oppenned');
-		})
-		$(document).delegate('.cls', 'click', function(event){
-			$('.header__menu').removeClass('oppenned');
-			event.stopPropagation();
-		});
+// Sticky Header
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 100) {
+        $('.header').addClass('sticky');
+    } else {
+        $('.header').removeClass('sticky');
+    }
 });
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.header').hasClass('open-nav')) {
+        $('.header').removeClass('open-nav');
+    } else {
+        $('.header').addClass('open-nav');
+    }
+});
+
+$('.header li a').click(function() {
+    if ($('.header').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.header').removeClass('open-nav');
+    }
+});
+
+// Navigation Scroll - ljepo radi materem
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
+
+
 
 var tabLinks = document.querySelectorAll(".services__menu a");
 var tabPanels = document.querySelectorAll(".services__desc--block");
